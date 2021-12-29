be0cc45784ce75bb9985ee98182c4910c4a4b900
coconutgit <43692301+coconutgit@users.noreply.github.com>
2021-12-29 20:50:55 +0000

    get lstm training work

--- ur5_kg_robot_yuyi/experiments/data/dict_to_lstm_data.py
+++ ur5_kg_robot_yuyi/experiments/data/dict_to_lstm_data.py
@@ -16,19 +16,19 @@ def note_to_num(note_name):
 with open('seq_data.txt', 'r') as f:
     lines = f.readlines()
     ctrl = np.zeros((100,12,10), dtype=float)
-    midi = np.zeros((100,12,5), dtype=int)
+    midi = np.zeros((100,12,5), dtype=float)
     current_group = 0
     current_sequence = np.zeros((1, 12, 10), dtype=float)
     for line in lines:
         dict = ast.literal_eval(line)
         l = list(dict.values())
         l[2] = note_to_num(l[2])
-        print(l)
+        # print(l)
         ctrl[l[0]][l[1]] = np.asarray(l[2:-1])
         # 'midi': [[[144, 69, 54, 0], 28854], [[128, 69, 86, 0], 29480]] --> [69, 54, 28854, 86, 29480] = [note, down_vel, down_time, up_vel, up_time]
         current_midi = l[-1]
         if current_midi != []:
-            midi[l[0]][l[1]] = [current_midi[0][0][1], current_midi[0][0][2], current_midi[0][1], current_midi[1][0][2], current_midi[1][1]]
+            midi[l[0]][l[1]] = [current_midi[0][0][1]/100, current_midi[0][0][2]/100, current_midi[0][1], current_midi[1][0][2]/100, current_midi[1][1]]
         # print(ctrl[l[0]][l[1]])
         # print(midi[l[0]][l[1]])
 
@@ -37,8 +37,19 @@ with open('seq_data.txt', 'r') as f:
 
     # ctrl[ctrl != np.array(None)]
     # midi[midi != np.array(None)]
-    print(ctrl)
-    print(midi)
+
+    for i in range(100):
+        start_time = midi[i][0][2]
+        for j in range(12):
+            if midi[i][j][2]!=0:
+                midi[i][j][2] -= start_time
+                midi[i][j][2] /= 10000
+            if midi[i][j][4]!=0:
+                midi[i][j][4] -= start_time
+                midi[i][j][4] /= 10000
+
+    np.save('lstm_ctrl', ctrl)
+    np.save('lstm_midi', midi)
 
 
 
--- ur5_kg_robot_yuyi/experiments/note.py
+++ ur5_kg_robot_yuyi/experiments/note.py
@@ -1,8 +1,8 @@
 class Note:
     """position information of keys on piano"""
-    i = 12345
-    position = 0.0
-    color = 'white'
+    # i = 12345
+    # position = 0.0
+    # color = 'white'
 
     def __init__(self, name, position, color):
         self.name = name
